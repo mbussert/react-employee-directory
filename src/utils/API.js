2c8325a817703b06d77f36a19d05f46f7b49bff7
Mike Bussert <mbussert@gmail.com>
2021-06-02 14:46:00 -0400

    Added Material-UI

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,15 +1,7 @@
 import axios from "axios";
 
-// Export an object containing methods we'll use for accessing the Dog.Ceo API
-
 export default {
-  getRandomDog: function() {
-    return axios.get("https://dog.ceo/api/breeds/image/random");
+  getRandomUser: function () {
+    return axios.get("https://randomuser.me/api/?results=25");
   },
-  getDogsOfBreed: function(breed) {
-    return axios.get("https://dog.ceo/api/breed/" + breed + "/images");
-  },
-  getBaseBreedsList: function() {
-    return axios.get("https://dog.ceo/api/breeds/list");
-  }
 };
