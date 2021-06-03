76b520af60b20a7315bfe0d6e146e683717d899c
Mike Bussert <mbussert@gmail.com>
2021-06-02 22:27:07 -0400

    Updated table component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Header from "./components/Header";
-import Wrapper from "./components/Wrapper";
+import DataTable from "./components/DataGrid";
 
 function App() {
   return (
     <Router>
-      <Header></Header>
       <div>
-        <Wrapper>
-          <Route exact path="/" component={Home} />
-        </Wrapper>
+        <DataTable></DataTable>
       </div>
     </Router>
   );
--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,13 @@
 import axios from "axios";
 
-export default {
-  getRandomUser: function () {
-    return axios.get("https://randomuser.me/api/?results=25");
-  },
-};
+// export default {
+//   getRandomUser: function () {
+//     return axios.get("https://randomuser.me/api/?results=25");
+//   },
+// };
+
+const instance = axios.create({
+  baseURL: "https://randomuser.me/api/?results=25",
+});
+
+export default instance;
